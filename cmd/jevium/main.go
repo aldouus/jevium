41b8c65ac5b22a0f4f4876e3ba9d0fc50a073649
Aldous <aldous@ndstudio.gov>
2026-09-22 19:12:32 -0400

    fix(cli): reject app navigation flags outside Appium

--- cmd/jevium/main.go
+++ cmd/jevium/main.go
@@ -45,6 +45,9 @@ func run(args []string) error {
 	if err := fs.Parse(args); err != nil {
 		return err
 	}
+	if *mode != "appium" && len(allowedApps) > 0 {
+		return fmt.Errorf("--allow-app requires appium mode")
+	}
 	if _, err := env.Require("TYPESAFE_API_KEY", "to call TypeSafe Jev"); err != nil {
 		return err
 	}
